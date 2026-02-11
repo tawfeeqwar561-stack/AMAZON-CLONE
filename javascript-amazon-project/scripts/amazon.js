daad61306100950a3a6f659572f84eff351fe8a6
tawfeeqwar561-stack <tawfeeqwar561@gmail.com>
2026-02-11 22:45:07 +0530

    make add to cart button interactive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -47,9 +47,32 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-name="${product.id}">
             Add to Cart
           </button>
         </div>`;
 });
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button, index) => {
+      button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+
+        let matchingItem;
+        cart.forEach((item) =>{
+          if (productId === item.productId) {
+            matchingItem = item;
+          }
+        });
+        if (matchingItem) {
+          matchingItem.quantity += 1;
+        } else {
+          cart.push({
+            productId: productId,
+            quantity: 1
+          });
+        }
+
+      });
+});
