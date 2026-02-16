bbf4946c7b4519ef16f2ced49203f648de118bc9
tawfeeqwar561-stack <tawfeeqwar561@gmail.com>
2026-02-16 17:01:09 +0530

    import products from products.js

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,6 +1,7 @@
 import {cart} from '../data/cart.js';
-let productsHTML = '';
+import {products} from '../data/products.js';
 
+let productsHTML = '';
 products.forEach((product) => {
     productsHTML += `
    <div class="product-container">
