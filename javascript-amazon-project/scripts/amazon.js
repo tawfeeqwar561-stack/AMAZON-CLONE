8c6206f0e9291f09f7288267bbfdd2f45e2f1312
tawfeeqwar561-stack <tawfeeqwar561@gmail.com>
2026-02-16 18:28:47 +0530

    Move addToCart into cart.js

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
@@ -57,35 +57,26 @@ products.forEach((product) => {
 });
  document.querySelector('.js-products-grid').innerHTML=productsHTML;
 
+        function updateCartQuantity() {
+          let cartQuantity = 0;
+
+        cart.forEach((cartItem) => {
+          cartQuantity += cartItem.quantity;
+
+        });
+         document.querySelector('.js-cart-quantity').textContent = cartQuantity; 
+  
+    }
+
    document.querySelectorAll('.js-add-to-cart').forEach((button, index) => {
       button.addEventListener('click', () => {
         const {productId} = button.dataset;
 
         const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-
         const quantity = Number(quantitySelector.value);
 
-        let matchingItem;
-        cart.forEach((item) =>{
-          if (productId === item.productId) {
-            matchingItem = item;
-          }
-      });
-        if (matchingItem) {
-          matchingItem.quantity += quantity;
-        } else {
-          cart.push({
-            productId,
-            quantity,
-          });
-        }
-        let cartQuantity = 0;
-
-        cart.forEach((item) => {
-          cartQuantity += item.quantity;
-
-        });
-       document.querySelector('.js-cart-quantity').textContent = cartQuantity; 
+        addToCart(productId, quantity);
+        updateCartQuantity();     
         const addedMessage = document.querySelector(
         `.js-added-to-cart-${productId}`
       );
@@ -94,5 +85,5 @@ products.forEach((product) => {
        setTimeout(() => {
         addedMessage.classList.remove('added-to-cart-visible');
       }, 2000);
+      });  
     });
-});
