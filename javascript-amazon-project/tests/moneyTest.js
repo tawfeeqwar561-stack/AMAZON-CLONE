5fa84e1badb9c91e3a863874c2eab22e788a72b8
tawfeeq imtiyaz war <tawfeeqwar561@gmail.com>
2026-04-04 11:21:20 +0530

    changed name from test-jasmine to tests and tests-simple inserted into tests by folder with some minor tweaks

--- javascript-amazon-project/tests/moneyTest.js
+++ javascript-amazon-project/tests/moneyTest.js
@@ -1,26 +1,15 @@
 import {formatCurrency} from '../scripts/utils/money.js';
 
-console.log('test suite: formatCurrency');
+describe('test suite: formatCurrency', ()=> {
+    it('converts cents into dollars', ()=> {
+        expect(formatCurrency(2095)).toEqual('20.95');
+        
+    });
+    it('works with 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
 
-console.log('converts cents into dollars')
-if (formatCurrency(2095) === '20.95') {
-    console.log('passed');
-} else {    
-    console.log('failed');
-}
-
-
-console.log('works with zero')
-if (formatCurrency(0)=== '0.00') {
-    console.log('passed');
-} else {
-    console.log('failed');
-}
-
-
-console.log('rounds up to the nearest cent')
-if (formatCurrency(2000.5)=== '20.01') {
-    console.log('passed');
-} else {
-    console.log('failed');
-}
+    it('rounds up to the nearest cent', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
--- javascript-amazon-project/tests/tests-simple/moneyTest.js
+++ javascript-amazon-project/tests/tests-simple/moneyTest.js
@@ -1,5 +1,4 @@
 import {formatCurrency} from '../scripts/utils/money.js';
-
 describe('test suite: formatCurrency', ()=> {
     it('converts cents into dollars', ()=> {
         expect(formatCurrency(2095)).toEqual('20.95');
