2ed312533e2d32fa26ef54a6d8bfe43a82d45878
tawfeeq imtiyaz war <tawfeeqwar561@gmail.com>
2026-04-06 16:47:01 +0530

    created order using the backend

--- javascript-amazon-project/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -2,6 +2,9 @@ import {cart} from '../../data/cart.js';
 import {getProduct} from '../../data/products.js';
 import {getDeliveryOption} from '../../data/deliveryOptions.js';
 import {formatCurrency} from '../utils/money.js';
+import {addOrder} from '../../data/orders.js';
+
+
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
@@ -59,7 +62,7 @@ export function renderPaymentSummary() {
     </div>
 
     <button class="place-order-button button-primary
-      js-place-order">
+      js-place-order js-place-order">
       Place your order
     </button>
   `;
@@ -70,23 +73,22 @@ export function renderPaymentSummary() {
   document.querySelector('.js-place-order')
     .addEventListener('click', async () => {
       try {
-        const response = await fetch('https://supersimplebackend.dev/orders', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            cart: cart
-          })
-        });
-
-        const order = await response.json();
-        addOrder(order);
-
-      } catch (error) {
-        console.log('Unexpected error. Try again later.');
-      }
+      const response =await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+      const order = await response.json();
+      addOrder(order);
+    } catch (error) {
+      console.log('Unexpected error. Try again later.');
+    }
 
       window.location.href = 'orders.html';
+
     });
 }
