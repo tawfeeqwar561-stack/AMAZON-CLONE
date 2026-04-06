8e38dfcaa6bdbd9ae2c04a8399c53e1eb85c309b
tawfeeq imtiyaz war <tawfeeqwar561@gmail.com>
2026-04-06 15:13:35 +0530

    added  backend practice file

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,7 +1,9 @@
-import {renderOrderSummary} from './checkout/orderSummary.js';
+import { loadProductsFetch } from '../data/products.js';
+import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-// import '../data/cart-class.js';
-// import '../data/backend-practice.js';
+import '../data/backend-practice.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProductsFetch().then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
